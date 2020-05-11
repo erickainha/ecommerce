ada98e4b83607b56ac0bbbf832e1f2ed935edbfc
ericka <erickaoliveira@msn.com>
2020-05-11 19:59:53 -0300

    tela de login

--- index.php
+++ index.php
@@ -1,19 +1,20 @@
 <?php 
-
+session_start();
 require_once("vendor/autoload.php");
+//require_once("functions.php");
 
 use \Slim\Slim;
 use \Hcode\Page;
 use \Hcode\PageAdmin;
+use \Hcode\Model\User;
 
-
-$app = new \Slim\Slim();
+$app = new Slim();
 
 $app->config('debug', true);
 
 $app->get('/', function() {
     
-	$page = new Hcode\Page();
+	$page = newPage();
 
 	$page->setTpl("index");
 
@@ -21,9 +22,40 @@ $app->get('/', function() {
 
 $app->get('/admin', function() {
     
+	User::verifyLogin();
+
 	$page = new PageAdmin();
 
 	$page->setTpl("index");
+
+});
+
+$app->get('/admin/login', function() {
+    
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+
+	$page->setTpl("login");
+
+});
+
+$app->post('/admin/login', function() {
+
+	User::login($_POST['login'], $_POST['password']);
+
+	header("Location: /admin");
+	exit;
+
+});
+
+$app->get('/admin/logout', function() {
+
+	User::logout();
+
+	header("Location: /admin/login");
+	exit;
 
 });
 
