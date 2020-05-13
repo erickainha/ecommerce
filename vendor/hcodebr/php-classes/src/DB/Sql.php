3645fd363f57cde787a0378441515f78dbd38b85
ericka <erickaoliveira@msn.com>
2020-05-13 09:04:47 -0300

    editar excluir usuarios

--- vendor/hcodebr/php-classes/src/DB/Sql.php
+++ vendor/hcodebr/php-classes/src/DB/Sql.php
@@ -63,6 +63,36 @@ class Sql {
 		return $stmt->fetchAll(\PDO::FETCH_ASSOC);
 
 	}
+	function post($key)
+	{
+		return str_replace("'", "", $_POST[$key]);
+	}
+	function get($key)
+	{
+		return str_replace("'", "", $_GET[$key]);
+	}
+
+	public function __call($name, $args)
+	{
+
+		$method = substr($name, 0, 3);
+		$fieldName = substr($name, 3, strlen($name));
+
+		switch ($method) {
+			case 'get':
+				return $this->values[$fieldName];
+				break;
+			
+			case 'set':
+				$this->values[$fieldName] = $args;
+				break;
+		}
+	}
+
+	public function lastInsertId() {
+
+    return $this->conn->lastInsertId();
+}
 
 }
 
--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -10,7 +10,7 @@ class User extends Model {
 	const SESSION = "User";
 
 	protected $fields = [
-		"iduser", "idperson", "deslogin", "despassword", "inadmin", "dtergister"
+		"iduser", "idperson", "deslogin", "despassword", "inadmin", "dtergister", "desperson", "desemail"
 	];
 
 	public static function login($login, $password):User
@@ -73,6 +73,77 @@ class User extends Model {
 
 	}
 
+	public static function listAll()
+	{
+		$sql = new Sql();
+
+		return $sql->select("SELECT * FROM td_users a INNER JOIN tb_persons b USING(idperson) ORDER BY b.desperson = :desperson");
+
+	}
+
+	public function save()
+	{
+		$sql = new Sql();
+		
+		$results = $sql->select("CALL sp_users_save(:desperson, :deslogin, :despassword, :desemail, :nrphone, :inadmin)", array(
+			":desperson"=>$this->getdesperson(),
+			":deslogin"=>$this->getdeslogin(),
+			":despassword"=>$this->getdespassword(),
+			":desemail"=>$this->getdesemail(),
+			":nrphone"=>$this->getnrphone(),
+			":inadmin"=>$this->getinadmin()
+		));
+
+		$this->setData($results[0]);
+	}
+
+	public function get($iduser)
+	{
+		$sql = new Sql();
+		$results = $sql->select("SELECT * FROM tb_users a INNER JOIN tb_persons b USING(idperson) WHERE a.iduser = :iduser", array(
+			":iduser"=>$iduser
+		));
+		$this->setData($results[0]);
+	}
+
+	public function update()
+	{
+		$sql = new Sql();
+		
+		$results = $sql->select("CALL sp_usersupdate_save(:iduser, :desperson, :deslogin, :despassword, :desemail, :nrphone, :inadmin)", array(
+			":iduser"=>$this->getiduser(),
+			":desperson"=>$this->getdesperson(),
+			":deslogin"=>$this->getdeslogin(),
+			":despassword"=>$this->getdespassword(),
+			":desemail"=>$this->getdesemail(),
+			":nrphone"=>$this->getnrphone(),
+			":inadmin"=>$this->getinadmin()
+		));
+
+		$this->setData($results[0]);
+	}
+
+	public function delete()
+	{
+
+		$sql = new Sql();
+		$sql->query("CALL sp_users_delete(:iduser)", array(
+			":iduser"=>this->getiduser()
+		));
+	}
+/*$sql->select("INSERT INTO tb_persons (desperson, desemail, nrphone) VALUES(:desperson, :desemail, :nrphone)", array(
+    ":desperson"=>$this->getdesperson(),
+    ":desemail"=>$this->getdesemail(),
+    ":nrphone"=>$this->getnrphone(),
+));
+ 
+$sql->select("INSERT INTO tb_users (idperson, deslogin, despassword, inadmin) VALUES(:idperson, :deslogin, :despassword, :inadmin)", array(
+    ":idperson"=>$sql->lastInsertId(),
+    ":deslogin"=>$this->getdeslogin(),
+    ":despassword"=>$this->getdespassword(),
+    ":inadmin"=>$this->getinadmin()
+));
+}*/
 }
 
  ?>
